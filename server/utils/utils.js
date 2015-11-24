332c32b5a78c1c8e4d6295325edbf2236228e96d
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-11-24 17:31:58 +0100

    Add description to utils.objectify

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -5,6 +5,15 @@ var Promise = require('bluebird');
 var chalk = require('chalk');
 var DataObjectParser = require('dataobject-parser')
 
+/**
+ * Returns a new object where property names
+ * with dots are converted into nested objects and arrays.
+ * 
+ * Example: { 'prop.sub': 'value' } -> { prop: { sub: value } }
+ * 
+ * @param {Array|Object} sqlArray
+ * @return {Array|Object}
+ */
 function objectify(sqlArray) {
   // Ensure it's an array
   var isObj;
@@ -16,11 +25,10 @@ function objectify(sqlArray) {
   var arr = _.map(sqlArray, function (sqlObj) {
     var d = new DataObjectParser();
     
+    // Get all values
     _.map(sqlObj, function (value, key) {
       d.set(key, value);
     });
-    
-    
     
     return d.data();
   });

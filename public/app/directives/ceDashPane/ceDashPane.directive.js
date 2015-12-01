b718ea7a7fb870cc9b7a6eeddde312a6315ec354
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-12-01 21:23:22 +0100

    Add aggregation of tickets to dashboard

--- public/app/directives/ceDashPane/ceDashPane.directive.js
+++ public/app/directives/ceDashPane/ceDashPane.directive.js
@@ -13,6 +13,18 @@ angular.module('customerEngineApp')
     },
     link: function (scope, element, attrs) {
       
+      scope.aggregated;
+      
+      function getAggregated(tickets) {
+        scope.aggregated = _.chain(tickets)
+          .groupBy('status')
+          .map(function (v, k) { return [ _.camelCase(k === 'Work in progress' ? 'wip' : k), v.length ] })
+          .zipObject()
+          .value();
+          
+        console.log(scope.aggregated);
+      }
+      
       /**
        * Watches for changes in tickets
        * and sets scope.pendingTickets to the tickets which are yet to be submitted.
@@ -22,6 +34,7 @@ angular.module('customerEngineApp')
           return !ticket.isSubmitted;
         });
         scope.tickets = tickets;
+        getAggregated(tickets);
       });
       
     }

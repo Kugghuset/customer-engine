38ffe6f89d9c3282563e15383657c98d5e99f421
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-12-09 11:28:02 +0100

    Load actual list of departments and show transferred to dep on card

--- server/api/department/sql/pop.department.initialize.sql
+++ server/api/department/sql/pop.department.initialize.sql
@@ -13,9 +13,40 @@ BEGIN
     [departmentName]
   )
   VALUES
-  ('Sales'),
-  ('IT'),
+  ('Customer Support'),
+  ('Payment Operation'),
+  ('Contract Operations'),
+  ('Logistics'),
+  ('Partner Service'),
   ('Marketing'),
-  ('Tech support'),
-  ('Customer support')
+  ('Product Operations'),
+  ('Sales'),
+  ('Visma')
+
 END
+
+IF NOT EXISTS(SELECT * FROM [dbo].[Department]
+              WHERE [departmentName] = 'Visma')
+  BEGIN
+    DROP TABLE [dbo].[Department]
+    
+    CREATE TABLE [dbo].[Department] (
+    [departmentId] bigint IDENTITY(1, 1) PRIMARY  KEY NOT NULL,
+    [departmentName] varchar(256) NULL
+    )
+  
+    INSERT INTO [dbo].[Department] (
+      [departmentName]
+    )
+    VALUES
+    ('Customer Support'),
+    ('Payment Operation'),
+    ('Contract Operations'),
+    ('Logistics'),
+    ('Partner Service'),
+    ('Marketing'),
+    ('Product Operations'),
+    ('Sales'),
+    ('Visma')
+    
+  END

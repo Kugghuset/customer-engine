75fd0d202b7c8f401f5dc116f85518e423397908
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-12-10 20:00:55 +0100

    Add search to views

--- public/app/services/Utils/Utils.service.js
+++ public/app/services/Utils/Utils.service.js
@@ -4,6 +4,31 @@
 angular.module('customerEngineApp')
 .factory('Utils', ['$q', '$http', function ($q, $http) {
   
+  
+  /**
+  * Escapes characters which need escaping in a RegExp.
+  * This allows for passing in any string into a RegExp constructor
+  * and have it seen as literal
+  * 
+  * @param {String} text
+  * @return {String}
+  */
+  function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s\/]/g, "\\$&");
+  };
+  
+  /**
+  * Returns an escaped RegExp object as the literal string *text*.
+  * Flags are optional, but can be provided.
+  * 
+  * @param {String} text
+  * @param {String} flags - optional
+  * @return {Object} - RegExp object
+  */
+  function literalRegExp(text, flags) {
+    return new RegExp(escapeRegex(text), flags);
+  }
+  
   var autoSaveState = [];
   
   return {
@@ -11,7 +36,9 @@ angular.module('customerEngineApp')
       return $q(function (resolve, reject) {
         console.log(obj);
       });
-    }
+    },
+    
+    literalRegExp: literalRegExp
   };
   
 }]);

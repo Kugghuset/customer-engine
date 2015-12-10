10af5b0df9b458a164800011a2923ce3d55bf6ac
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-12-10 18:43:53 +0100

    Display owner of ticket on cards

---
+++ public/app/directives/ceUserCircle/ceUserCircle.directive.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict'
+
+angular.module('customerEngineApp')
+.directive('ceUserCircle', ['$location', 'Auth', 'Notification', function ($location, Auth, Notification) {
+  return {
+    templateUrl: 'app/directives/ceUserCircle/ceUserCircle.html',
+    restrict : 'EA',
+    scope: {
+      user: '='
+    },
+    link: function (scope, element, attrs) {
+      
+      scope.getContent = function (user) {
+        
+        if (!user) { return ''; }
+        
+        if (user.name) {
+          
+          return _.map(user.name.split(' '), function (part) {
+          return _.first(part);
+        }).join('').toUpperCase()
+          
+        }
+      };
+    }
+  }
+}]);
+
+})();

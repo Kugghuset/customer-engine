71404925fce329502caa3823ab30b27390859802
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-12-17 15:10:29 +0100

    Disallow agents changing to existing email addressing

--- server/api/user/sql/user.update.sql
+++ server/api/user/sql/user.update.sql
@@ -6,13 +6,20 @@ SET XACT_ABORT ON
 
 BEGIN TRAN
 
-UPDATE [dbo].[User]
-SET
-  [dbo].[User].[email] = @email,
-  [dbo].[User].[name] = @name,
-  [dbo].[User].[departmentId] = @departmentId,
-  [dbo].[User].[dateChanged] = GETUTCDATE()
-
-WHERE [userId] = @userId
-
+IF EXISTS(SELECT * FROM [dbo].[User]
+          WHERE [email] = @email
+            AND [userId] != @userId)
+BEGIN
+  SELECT 'Email already exists.' AS [error]
+END
+ELSE
+  UPDATE [dbo].[User]
+  SET
+    [dbo].[User].[email] = @email,
+    [dbo].[User].[name] = @name,
+    [dbo].[User].[departmentId] = @departmentId,
+    [dbo].[User].[dateChanged] = GETUTCDATE()
+  
+  WHERE [userId] = @userId
+  
 COMMIT TRAN

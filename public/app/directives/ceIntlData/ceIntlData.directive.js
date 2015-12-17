279baa2853099cb5cc5cf08566c53daaf4d88095
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-12-17 16:17:55 +0100

    Match country and tel country

---
+++ public/app/directives/ceIntlData/ceIntlData.directive.js
@@ -0,0 +1,38 @@
+/* global moment */
+/* global rome */
+(function () {
+'use strict'
+
+angular.module('customerEngineApp')
+.directive('ceIntlData', ['$timeout', 'Country', function ($timeout, Country) {
+  return {
+    restrict : 'A',
+    scope: {
+      country: '='
+    },
+    link: function (scope, element, attrs) {
+      
+      /**
+       * Watches for changes in the country variable on scope.
+       */
+      scope.$watch('country', function (newVal, oldVal) {
+        if (_.isObject(newVal)) {
+          element.intlTelInput("selectCountry", newVal.short);
+        }
+      });
+      
+      /**
+       * Watches for changes in the selected country data
+       * from the flag dropdown.
+       */
+      scope.$watch(function () {
+        return element.intlTelInput("getSelectedCountryData");
+      }, function (newVal, oldVal) {
+        // Assign the country variable to the selected country from the country list.
+        scope.country = _.find(Country.getShortAndNames(), { short: newVal.iso2.toUpperCase() }) || scope.country;
+      });
+    }
+  }
+}]);
+
+})();

822e9a140de2a9df462b20e1f96bba82bde4dfd9
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-02-04 12:26:52 +0100

    Update bulk import to perform a merge on the datepart of npsDate and the npsTel

--- server/api/nps/sql/nps.bulkImport.sql
+++ server/api/nps/sql/nps.bulkImport.sql
@@ -1,3 +1,4 @@
+
 /*
 Inserts the npss in bulk from the file provided in the file.
 */
@@ -6,26 +7,18 @@ SET XACT_ABORT ON
 
 BEGIN TRAN
 
-DECLARE @Dates Table (
-  [npsDate] datetime2 NULL
-)
-INSERT INTO @Dates
-SELECT [npsDate]
-FROM [tickety].[dbo].[NPS]
-GROUP BY [npsDate]
-
 -- Drop the TempNPS if it exists
 IF (Object_ID('TempNPS', 'U') IS NOT NULL)
 BEGIN
-  DROP TABLE [dbo].[TempNPS]
+    DROP TABLE [dbo].[TempNPS]
 END
 
 -- Create temp table
 CREATE TABLE [dbo].[TempNPS] (
-  [rawNpsDate] varchar(256) NULL, -- will be converted later on
-  [npsTel] varchar(256) NULL,
-  [npsScore] varchar(256) NULL,
-  [npsComment] varchar(max) NULL,
+    [rawNpsDate] varchar(256) NULL -- will be converted later on
+  , [npsTel] varchar(256) NULL
+  , [npsScore] varchar(256) NULL
+  , [npsComment] varchar(max) NULL
 )
 
 -- filepath i set in JavaScript
@@ -34,11 +27,11 @@ INSERT [dbo].[TempNPS]
 FROM '{ filepath }'
 WITH
 (
-  FIRSTROW = 3, -- Skip the column name row
-  FIELDTERMINATOR = ';',
-  ROWTERMINATOR = '\n',
-  DATAFILETYPE = 'widechar', -- This part is super important for åäö
-  CODEPAGE = 'RAW' -- Supposedly this one too
+    FIRSTROW = 2 -- Skip the column name row
+  , FIELDTERMINATOR = ';'
+  , ROWTERMINATOR = '\n'
+  , DATAFILETYPE = 'widechar' -- This part is super important for åäö
+  , CODEPAGE = 'RAW' -- Supposedly this one too
 )
 
 -- Add the response field
@@ -52,16 +45,54 @@ SET [npsDate] = CASE
   ELSE ''
 END
 
--- Insert into NPS
-INSERT INTO [dbo].[NPS] (
-  [npsDate],
-  [npsTel],
-  [npsScore],
-  [npsComment]
+INSERT INTO [Tickety].[dbo].[NPS] (
+    [npsDate]
+  , [npsTel]
+  , [npsScore]
+  , [npsComment]
 )
-SELECT [npsDate], [npsTel], [npsScore], [npsComment]
-FROM [dbo].[TempNPS]
-WHERE [npsDate] NOT IN (SELECT * FROM @Dates)
+SELECT
+    [npsDate]
+  , [npsTel]
+  , [npsScore]
+  , [npsComment]
+FROM (
+    MERGE [Tickety].[dbo].[NPS] AS [Target]
+    USING [dbo].[TempNPS] AS [Source]
+    ON
+        -- Match on the npsTel and the date only of the npsDate
+        [Target].[npsTel] = [Source].[npsTel]
+    AND CAST([Target].[npsDate] AS date) = CAST([Source].[npsDate] AS date)
+    
+    WHEN MATCHED AND (
+        [Target].[npsScore] != [Source].[npsScore]
+     OR [Target].[npsComment] != [Source].[npsComment]
+     OR [Target].[isLocal] = 1
+    )
+    THEN UPDATE SET
+        [Target].[npsScore] = [Source].[npsScore]
+      , [Target].[npsComment] = [Source].[npsComment]
+      , [Target].[isLocal] = NULL
+      , [Target].[dateChanged] = GETUTCDATE()
+
+    WHEN NOT MATCHED BY TARGET
+        THEN INSERT (
+            [npsDate]
+          , [npsTel]
+          , [npsScore]
+          , [npsComment]
+        )
+        VALUES (
+            [Source].[npsDate]
+          , [Source].[npsTel]
+          , [Source].[npsScore]
+          , [Source].[npsComment]
+        )
+
+    OUTPUT $action AS [Action], [Source].*
+)    AS [MergeOutput]
+ WHERE [MergeOutput].[Action] = NULL
+
 
 -- Drop the temp table
 DROP TABLE [dbo].[TempNPS]

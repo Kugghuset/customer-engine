372804b5e9585384b3b4e3eb2325168700a778f0
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-02-12 10:18:44 +0100

    Add roles to login as well

--- server/api/user/sql/user.updateLastLoggedIn.sql
+++ server/api/user/sql/user.updateLastLoggedIn.sql
@@ -9,19 +9,24 @@ WHERE [dbo].[User].[userId] = @userId
 
 -- Select user on new
 SELECT
-  [U].[userId],
-  [U].[email],
-  [U].[name],
-  [U].[password],
-  [U].[lastLoggedIn],
-  [D].[departmentId] AS [department.departmentId],
-  [D].[departmentName] AS [department.departmentName],
-  [D].[country] AS [department.country]
+    [U].[userId]
+  , [U].[email]
+  , [U].[name]
+  , [U].[password]
+  , [U].[lastLoggedIn]
+  , [D].[departmentId] AS [department.departmentId]
+  , [D].[departmentName] AS [department.departmentName]
+  , [D].[country] AS [department.country]
+  , [R].[role]
+  , [R].[roleName]
   
 FROM [dbo].[User] AS [U]
 
 -- Join on departmentId
 LEFT JOIN [dbo].[Department] AS [D]
 ON [U].[departmentId] = [D].[departmentId]
+
+LEFT JOIN [dbo].[Role] AS [R]
+ON [U].[role] = [R].[role]
 
 WHERE [U].[userId] = @userId
--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -31,8 +31,6 @@ exports.login = function (req, res) {
   // No user, so we can't login.
   if (!req.body) { return utils.handleError(res, new Error('No user provided')); }
   
-  
-  
   //Authenticate user with submitted email and password
   User.auth(req.body.email, req.body.password)
   .then(function (user) {

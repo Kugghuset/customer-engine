128c1631e3965c4bed8d120b9d5375521db50d91
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-02-29 11:53:04 +0100

    Hide tickets with incorrect closed status in call back view

--- public/app/directives/ceCallBackGrid/ceCallBackGrid.directive.js
+++ public/app/directives/ceCallBackGrid/ceCallBackGrid.directive.js
@@ -11,6 +11,12 @@ angular.module('ticketyApp')
     },
     link: function (scope, element, attrs) {
       
+      function getStatusOfPage() {
+        return /false/.test(($location.search() || {}).isClosed)
+          ? false
+          : true;
+      }
+      
       /**
        * Splits the comment on new lines after converting \\n to \n.
        * 
@@ -32,9 +38,20 @@ angular.module('ticketyApp')
        * @return {Array}
        */
       scope.getSort = function () {
-        return /false/.test(($location.search() || {}).isClosed)
-          ? ['ticketDate', 'ticketId']
-          : ['callBack.dateClosed', 'ticketId'];
+        return getStatusOfPage()
+          ? ['callBack.dateClosed', 'ticketId']
+          : ['ticketDate', 'ticketId'];
+          
+      };
+      
+      /**
+       * Returns true or false for whether the object should be hidden or not.
+       * 
+       * @param {Object} ticket
+       * @return {Boolean}
+       */
+      scope.shouldHide = function (ticket) {
+        return getStatusOfPage() != !!_.get(ticket, 'callBack.isClosed');
       };
       
     }

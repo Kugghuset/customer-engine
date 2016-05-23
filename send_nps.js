677d604b48d549c5ebc1474cc3a2e1222095dd6b
Kristoffer Östlund <kr0201889@cidronpay.com>
2016-05-23 15:56:16 +0200

    Add support for setting the week via cli when sendin nps

--- send_nps.js
+++ send_nps.js
@@ -6,6 +6,8 @@ var Promise = require('bluebird');
 var sql = require('seriate');
 var bodyParser = require('body-parser');
 var chalk = require('chalk');
+var fs = require('fs');
+var path = require('path');
 
 var config = require('./server/config/config');
 var logger = require('./server/utils/logger.util')
@@ -15,7 +17,37 @@ sql.setDefaultConfig(config.db);
 
 var nps = require('./server/services/nps').module;
 
-nps.sendSurway(0)
+// Get the args
+var args = process.argv.map(function (val, index, array) { return val }).slice(2);
+
+var options = {}
+var currentKey;
+
+// Iterate over all args to populate *options*
+args.forEach(function (val) {
+  // Set current key if *val* starts with -[-]
+  if (/^\-+/.test(val)) {
+    currentKey = val.replace(/^\-+/, '');
+    // At least append the key
+    return options[currentKey] = undefined;
+  }
+
+  options[currentKey] = (typeof options[currentKey] === 'undefined')
+    ? val
+    : options[currentKey] + ' ' + val;
+})
+
+/************************
+ *
+ * Command handlers
+ *
+ ***********************/
+
+var weeks = _.isUndefined(options.weeks)
+  ? 0
+  : parseInt(options.weeks);
+
+nps.sendSurway(weeks || 0)
 .then(function (data) {
   console.log(chalk.green(
     '{num} messages sent.'.replace('{num}', _.get(data, 'length') || 0)

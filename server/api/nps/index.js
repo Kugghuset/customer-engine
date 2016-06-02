28944ce788e189c0ceb1599d1d878598b196cabc
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-06-02 13:49:44 +0200

    Add little test

--- server/api/nps/index.js
+++ server/api/nps/index.js
@@ -3,7 +3,15 @@
 var express = require('express');
 var NPS = require('./nps.db');
 var controller = require('./nps.controller');
+var moment = require('moment');
+
 
 var router = express.Router();
+
+var _date = new Date();
+
+router.get('/', function (req, res) {
+  res.status(200).json({ date: moment(_date).format('YYYY-MM-DD HH:mm') });
+});
 
 module.exports = router;

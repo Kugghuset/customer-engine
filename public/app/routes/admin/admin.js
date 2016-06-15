df0683431519f0d6e73c20fb3a871924cf7e2bf4
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-06-15 14:47:22 +0200

    Fix styling for dropdowns and fix bug In country list

--- public/app/routes/admin/admin.js
+++ public/app/routes/admin/admin.js
@@ -33,9 +33,24 @@ angular.module('ticketyApp')
   $scope.updateFilters = function (options) {
     var opts = CallBack.setOptions({
       userId: _.get(options, 'user.userId'),
-      groupingCountry: _.get(options, 'groupingCountry'),
+      groupingCountry: _.get(options, 'country.shortcode'),
     });
   }
+
+  $scope.setCountry = function (country) {
+    if (!$scope.filterOptions) { $scope.filterOptions = {}; }
+
+    $scope.filterOptions.country = country;
+
+    console.log($scope.filterOptions);
+  }
+
+  $scope.countries = [
+    { name: 'Sweden', shortcode: 'SE' },
+    { name: 'Denmark', shortcode: 'DK' },
+    { name: 'Norway', shortcode: 'NO' },
+    { name: 'Finland', shortcode: 'FI' },
+  ]
 
   $scope.$watch(function () {
     return $state.current;
